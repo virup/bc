8f69d3863fc0c7f218e0e93f75de2c1e2f9b8991
Rahul Batra <rahul.batra.in@gmail.com>
2017-01-22 14:17:06 -0800

    Added Archon.java

---
+++ src/SpartanPlayer/Archon.java
@@ -0,0 +1,60 @@
+package SpartanPlayer;
+import battlecode.common.*;
+
+/**
+ * Created by rahubatr on 1/22/17.
+ */
+public strictfp class Archon {
+
+    private void createGardener() {
+
+    }
+
+    private void moveRandomly() {
+
+    }
+
+    private void moveInADirection(Direction dir) {
+
+    }
+
+    private void moveInARectangle() {
+
+    }
+
+    private int numStridesSinceLastGardenerHired() {
+        return 0;
+    }
+
+    private void exchangeBulletsForVictoryPoints() {
+
+    }
+
+    private void shouldExchangeBulletsForVictoryPoints() {
+    }
+
+    // broadcast to other robots that I am alive
+    private void setArchonAlive() {
+    }
+
+    // is under attack
+    private void isUnderAttack() {
+    }
+
+    private int getNumberOfArchonsOnMap() {
+        return 0;
+
+    }
+
+    private int getNumberOfGardeners() {
+        return 0;
+    }
+
+    // Sight Radius = 10 for Archon
+    // Can broadcast to other robots in the team
+    private MapLocation isEnemyCloseBy(RobotController rc) {
+        return rc.getLocation();
+    }
+
+}
+
